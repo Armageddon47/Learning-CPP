0863311f2b6e798a8c0e563896fff9c28b05f4eb
Armageddon47 <123872869+Armageddon47@users.noreply.github.com>
2023-02-08 19:31:48 +0400

    working on it

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,19 +1,93 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 #include "product.h"
-
 using namespace std;
 using namespace production;
 
-double product_prices[4];
-string product_names[4];
+enum State
+{
+	Add,
+	Sort,
+	Display,
+	Exit
+};
+
+struct Products
+{
+	string Name;
+	double Price;
+
+
+	Products AddItem()
+	{
+		Products temp;
+
+		cout << "Product Name\n";
+		getline(cin, temp.Name);
+
+		cout << "Price?\n";
+		cin >> temp.Price;
+		cout << temp.Name << "\t" << temp.Price << "Added";
+		return temp;
+	}
+
+
+
+
+};
+
+void DisplayProducts(vector<Products>temp)
+{
+	
+	int i = 0;
+	while (i<temp.size())
+	{
+		cout << "Name :" << temp[i].Name << " Price :" << temp[i].Price;
+		i++;
+	}
+}
+Products SortItems(vector<Products>temp)
+{
+	int i = 0;
+	Products expensive = temp[0];
+	vector<Products>output;
+	while (true)
+	{
+		for (i = 0; i<temp.size();i++)
+		{
+			if (temp[i].Price>expensive.Price)
+			{
+				expensive = temp[i];
+			}
+		}
+		output.push_
+	}
+}
 int main()
 {
+	State state = Display;
+	vector<Products> products;
+
+	//adding items
+	while (true)
+	{
+		Products temp;
+		char input;
+		cout << "wanna enter products? y/n \n";
+		cin >> input;
+		if (input == 'y')
+		{
+			temp.AddItem();
+			products.push_back(temp);
+		}
+		else
+			break;
+	}
     
-    cout << "Welcome\n";
-    AddProducts();
     SortProducts();
     Purchase();
+    
 
 
     return 0;
